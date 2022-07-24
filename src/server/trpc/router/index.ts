1eebaf96345f6b2b89136f1adb306d190f1de61d
galnir <nirnir122@gmail.com>
2022-07-24 17:32:36 +0300

    workouts router

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,9 +1,11 @@
 // src/server/trpc/router/index.ts
 import { t } from "../utils";
+import { workoutsRouter } from "./workouts";
 import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 
 export const appRouter = t.router({
+  workout: workoutsRouter,
   example: exampleRouter,
   auth: authRouter,
 });
---
+++ src/server/trpc/router/workouts.ts
@@ -0,0 +1,64 @@
+import { t } from "../utils";
+import { z } from "zod";
+
+import { authedProcedure } from "../utils";
+
+export const workoutsRouter = t.router({
+  submit: t.procedure
+    .input(
+      z.object({
+        userId: z.string(),
+        name: z.string(),
+        description: z.string().min(5).max(100).nullable(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { userId, name, description } = input;
+      const workout = await ctx.prisma.workout.create({
+        data: {
+          userId,
+          name,
+          description,
+        },
+      });
+
+      return {
+        workout,
+      };
+    }),
+
+  getAll: authedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const workouts = await ctx.prisma.workout.findMany({
+        where: {
+          userId: input.userId,
+        },
+      });
+
+      return {
+        workouts,
+      };
+    }),
+
+  getById: authedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const workout = await ctx.prisma.workout.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          exercises: true,
+          name: true,
+          description: true,
+          id: true,
+          user: true,
+        },
+      });
+
+      return {
+        workout,
+      };
+    }),
+});
